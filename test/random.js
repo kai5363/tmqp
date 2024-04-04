bc8cf537148a46e42a7428fccfe5ff7e490fc5fb
Kelvin Wu <c.kelvin.wu@gmail.com>
2024-04-04 15:31:03 -0700

    refactor: remove redundant console log

--- test/random.js
+++ test/random.js
@@ -1,8 +1,22 @@
 const Tmqp = require('../tmqp');
 
-const tmqp = new Tmqp({ host: 'localhost', port: 3006, cluster: true });
-const students = ['Kelvin', 'Peter', 'Adam', 'Hazel', 'Howard', 'Claudia', 'Alex',
-  'Tim', 'Sam', 'Euli', 'Ellie', 'Domingo', 'Jimmy', 'Morton'];
+const tmqp = new Tmqp({ host: 'localhost', port: 25566, cluster: true });
+const students = [
+  'Kelvin',
+  'Peter',
+  'Adam',
+  'Hazel',
+  'Howard',
+  'Claudia',
+  'Alex',
+  'Tim',
+  'Sam',
+  'Euli',
+  'Ellie',
+  'Domingo',
+  'Jimmy',
+  'Morton',
+];
 
 const getStudents = (nums) => {
   const studentList = [];
@@ -18,15 +32,27 @@ const randomProduce = (queue) => {
   const interval = Math.random() * 2000;
   const studentList = getStudents(times);
   tmqp.produce(queue, studentList);
+  const d = new Date();
+  console.log(
+    d.toLocaleDateString('en-US'),
+    d.toLocaleTimeString('en-US'),
+    `[produce] queue:${queue}, list:${JSON.stringify(studentList)}`,
+  );
   setTimeout(() => {
     randomProduce(queue);
   }, interval);
 };
 
-const randomConsume = (queue) => {
+const randomConsume = async (queue) => {
   const times = Math.floor(Math.random() * 10) + 1;
   const interval = Math.random() * 2000;
-  tmqp.consume(queue, times);
+  const result = await tmqp.consume(queue, times);
+  const d = new Date();
+  console.log(
+    d.toLocaleDateString('en-US'),
+    d.toLocaleTimeString('en-US'),
+    `[consume] queue:${queue}, list:${JSON.stringify(result)}`,
+  );
 
   setTimeout(() => {
     randomConsume(queue);
